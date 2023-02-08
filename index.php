803a4a968ab210de142395ac9877a2d2d5e6693c
Wansaaa <ichwansanusi60@gmail.com>
2023-02-09 05:26:57 +0700

    Flex Box pada CSS

--- index.php
+++ index.php
@@ -3,23 +3,28 @@
         $bil1 = $_POST['bil1'];
         $bil2 = $_POST['bil2'];
         $operasi = $_POST['operasi'];
-    
         switch ($operasi) {
-            case 'tambah':
-                $hasil = $bil1+$bil2;   
+            case '+':
+                $hasil = $bil1+$bil2;
             break;
-            case 'kurang':
+            case '-':
                 $hasil = $bil1-$bil2;
             break;
-            case 'kali':
+            case 'x':
                 $hasil = $bil1*$bil2;
             break;
-            case 'bagi':
+            case ':':
                 $hasil = $bil1/$bil2;
+            break;
+            case '%':
+                $hasil = $bil1%$bil2;
+            break;
+            case '**':
+                $hasil = $bil1**$bil2;
             break;
         }
     }
-    ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,34 +33,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kalkulator PHP Ichwan Sanusi</title>
-    <link rel="stylesheet" type="text/css" href="style.css">    
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<div class="kalkulator">
-    <h2 class="judul">Kalkulator PHP Ichwan Sanusi</h2>
-    <form method="post" action="index.php">
-        <input type="number" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama">
-        <input type="number" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
-        <select class="opt" name="operasi">
-            <option value="tambah">+</option>
-            <option value="kurang">-</option>
-            <option value="kali">x</option>
-            <option value="bagi">:</option>
-        </select>
-        <input type="submit" name="hitung" value="Hitung" class="tombol">
-    </form>
-    <div class="answer">
-        <center>
-            <?php if(isset($_POST['hitung'])){ ?>
-                <input type="text" value="<?php echo $hasil; ?>" class="bil">
-            <?php }else{ ?>
-                <input type="text" disabled class="bil" value="0">
-            <?php } ?>
-        </center>
+    <div class="container">
+        <div class="kalkulator">
+            <h2 class="judul">Kalkulator PHP <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-calculator" viewBox="0 0 16 16">
+            <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+            <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"/>
+            </svg> Ichwan Sanusi</h2>
+            <form method="post" action="index.php">
+                <input type="number" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama" required>
+                <input type="number" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua" required>
+                <select class="opt" name="operasi" required>
+                    <option value="">--Pilih--</option>
+                    <option value="+">Tambah (+)</option>
+                    <option value="-">Kurang (-)</option>
+                    <option value="x">Kali (x)</option>
+                    <option value=":">Bagi (:)</option>
+                    <option value="%">Sisa Bagi (%)</option>
+                    <option value="**">Pemangkatan (**)</option>
+                </select>
+                <button type="submit" name="hitung" class="tombol">Hitung</button> 
+            </form>
+            <div class="answer">
+                <center>
+                    <?php if(isset($_POST['hitung'])){?>
+                        <input type="number" value="<?php echo$hasil;?>" class="bil" >
+                    <?php }else{ ?>
+                        <input type="number" disabled class="bil" value="0">
+                        <?php } ?>
+                </center>
+            </div>
+            <div class="footer">
+                <p>&copy; copyright 2022 | <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                </svg> by. <a href="https://www.instagram.com/Ichwan_sanusi/" target="_blank">Ichwan Sanusi</a></p>
+            </div>  
+        </div>
     </div>
-</div>
 
-    
 </body>
 </html>
